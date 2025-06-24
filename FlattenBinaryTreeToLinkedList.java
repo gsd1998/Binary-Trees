657d46d5ab551571614311e21cceb0ac5a59fa8d
Giri Shanker D <43494225+gsd1998@users.noreply.github.com>
2025-06-25 00:57:05 +0530

    Add files via upload

---
+++ FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,15 @@
+package interviewQA.Trees.BinaryTrees;
+
+public class FlattenBinaryTreeToLinkedList {
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
